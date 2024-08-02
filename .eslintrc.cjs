0329f75f7579e307d5b037d26dbd0e55cbec1572
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2024-08-02 20:08:20 +0100

    fixed bugs on navbar

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true, // Ensure Node.js environment is included
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -18,4 +22,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
