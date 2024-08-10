cd62895eccf4a90cbc1ad71ddb4587e216c6d641
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2024-08-10 23:53:14 +0100

    added styled on dashboard page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
 
 const Footer = () => {
-  useEffect(() => {
-    const footer = document.querySelector('.footer');
+  const [visible, setVisible] = useState(false);
+  const footerRef = useRef(null);
 
+  useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.innerHeight + window.scrollY;
       const documentHeight = document.documentElement.offsetHeight;
-      
-      console.log('Scroll Position:', scrollPosition);
-      console.log('Document Height:', documentHeight);
 
       if (scrollPosition >= documentHeight) {
-        footer.classList.add('visible');
+        setVisible(true);
       } else {
-        footer.classList.remove('visible');
+        setVisible(false);
       }
     };
 
-    //window.addEventListener('scroll', handleScroll);
+    const handleClickOutside = (event) => {
+      if (footerRef.current && !footerRef.current.contains(event.target)) {
+        setVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('click', handleClickOutside);
+
+    // Initial check in case the user is already at the bottom
+    handleScroll();
 
     return () => {
-      //window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('click', handleClickOutside);
     };
   }, []);
 
   return (
-    <footer className="footer">
+    <footer
+      className={`footer ${visible ? 'visible' : ''}`}
+      ref={footerRef}
+    >
       <div className="footer-content">
         <p>&copy; 2024 Local Handicraft. All Rights Reserved.</p>
         <ul className="footer-links">
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect, useContext, useRef } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'; 
 import { AuthContext } from '../context/auth.context';
-import './Dashboard.css'; 
+import ProductForm from '../components/ProductForm';
+import './Dashboard.css'; // Import the CSS file for styling
 
 function Dashboard() {
   const { user } = useContext(AuthContext);
-  const [showForm, setShowForm] = useState(false);
   const [products, setProducts] = useState([]);
   const [formData, setFormData] = useState({
     name: '',
     description: '',
     price: '',
     imageUrl: '',
-    category: 'Clothing', 
+    category: 'Clothing',
     stock: ''
   });
   const [editProductId, setEditProductId] = useState(null);
@@ -24,8 +23,6 @@ function Dashboard() {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      if (!user?.id) return;
-
       try {
         const response = await axios.get('http://localhost:5005/api/products', {
           params: { userId: user.id }
@@ -50,14 +47,7 @@ function Dashboard() {
     }
   }, [scrollTarget]);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    
+  const handleSubmit = async () => {
     const processedFormData = {
       ...formData,
       price: Number(formData.price),
@@ -85,7 +75,6 @@ function Dashboard() {
         stock: ''
       });
       setEditProductId(null);
-      setShowForm(false);
 
       // Refresh the product list
       const updatedResponse = await axios.get('http://localhost:5005/api/products', {
@@ -137,87 +126,20 @@ function Dashboard() {
   return (
     <div className="dashboard">
       <header className="dashboard-header">
-        <h1>Hello, {user?.name}! We are glad to have you back.</h1>
+        <h3>Hello, {user?.name}! We are glad to have you back.</h3>
       </header>
 
       <main className="dashboard-content">
-        <section ref={formRef} className="form-section">
-          <h2>Sell Your Product</h2>
-          <p>Ready to make some money? Post your items for sale and reach a wider audience.</p>
-          <button className="post-item-button" onClick={handlePostItemClick}>
-            {showForm ? 'Cancel' : 'Post Item'}
-          </button>
-
-          {showForm && (
-            <form className="post-item-form" onSubmit={handleSubmit}>
-              <label>
-                Product Name:
-                <input
-                  type="text"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleInputChange}
-                  required
-                />
-              </label>
-              <label>
-                Description:
-                <textarea
-                  name="description"
-                  value={formData.description}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <label>
-                Price:
-                <input
-                  type="number"
-                  name="price"
-                  value={formData.price}
-                  onChange={handleInputChange}
-                  required
-                />
-              </label>
-              <label>
-                Image URL:
-                <input
-                  type="text"
-                  name="imageUrl"
-                  value={formData.imageUrl}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <label>
-                Category:
-                <select
-                  name="category"
-                  value={formData.category}
-                  onChange={handleInputChange}
-                >
-                  <option value="Electronics">Electronics</option>
-                  <option value="Books">Books</option>
-                  <option value="Clothing">Clothing</option>
-                  <option value="Furniture">Furniture</option>
-                  <option value="Toys">Toys</option>
-                </select>
-              </label>
-              <label>
-                Stock:
-                <input
-                  type="number"
-                  name="stock"
-                  value={formData.stock}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <button type="submit">{editProductId ? 'Update' : 'Submit'}</button>
-              {error && <p className="error-message">{error}</p>}
-            </form>
-          )}
-        </section>
-
+        <ProductForm 
+          formData={formData} 
+          setFormData={setFormData} 
+          editProductId={editProductId} 
+          setEditProductId={setEditProductId} 
+          onSubmit={handleSubmit} 
+          error={error} 
+          successMessage={editProductId ? 'Your product is updated successfully' : 'You have posted your product successfully'}
+        />
         <section className="product-list-section">
-          <h2>Product List</h2>
           <ul className="product-list">
             {products.length > 0 ? (
               products.map(product => (
@@ -226,14 +148,18 @@ function Dashboard() {
                   ref={el => (productRefs.current[product._id] = el)} // Assign ref to product item
                   className={`product-item ${product._id === scrollTarget ? 'highlight' : ''}`}
                 >
-                  <h3>{product.name}</h3>
-                  <p>{product.description}</p>
-                  <p>Price: ${product.price}</p>
-                  <p>Category: {product.category}</p>
-                  <p>Stock: {product.stock}</p>
-                  {product.imageUrl && <img src={product.imageUrl} alt={product.name} className="product-image" />}
-                  <button onClick={() => handleEditClick(product)} className="edit-button">Edit</button>
-                  <button onClick={() => handleDeleteClick(product._id)} className="delete-button">Delete</button>
+                  <div className="product-info">
+                    <h3>{product.name}</h3>
+                    <p>{product.description}</p>
+                    <p>Price: ${product.price}</p>
+                    <p>Category: {product.category}</p>
+                    <p>Stock: {product.stock}</p>
+                    {product.imageUrl && <img src={product.imageUrl} alt={product.name} className="product-image" />}
+                  </div>
+                  <div className="product-actions">
+                    <button onClick={() => handleEditClick(product)} className="edit-button">Edit</button>
+                    <button onClick={() => handleDeleteClick(product._id)} className="delete-button">Delete</button>
+                  </div>
                 </li>
               ))
             ) : (
