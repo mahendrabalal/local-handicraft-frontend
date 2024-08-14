345c4050201c4fa9596928da85a3a0d54c9067e3
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2024-08-14 21:56:37 +0100

    fix bugs at index.html

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+if (import.meta.env.MODE === 'development') {
+    // Development-specific code
+    console.log('Running in development mode');
+  } else {
+    // Production-specific code
+    console.log('Running in production mode');
+  }
+  
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,18 @@ import App from './App.jsx';
 import './index.css';
 import { AuthProviderWrapper } from './context/auth.context.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './config.js'
 
+// Handle environment-specific logic
+if (import.meta.env.MODE === 'development') {
+  console.log('Running in development mode');
+  // Development-specific code
+} else {
+  console.log('Running in production mode');
+  // Production-specific code
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 root.render(
   <React.StrictMode>
