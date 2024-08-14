5117e923cb3bdf909a8d031b35771cfd1a43b2f9
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2024-08-15 00:30:16 +0100

    fixed bugs on index.html

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-if (import.meta.env.MODE === 'development') {
-    // Development-specific code
-    console.log('Running in development mode');
-  } else {
-    // Production-specific code
-    console.log('Running in production mode');
-  }
-  
