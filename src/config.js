643220ccfd9fbb70bfeaf8e6acbdebf5c339fb31
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2024-08-18 12:43:11 +0100

    change API_URL to fix connection issue in netlify

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // config.js
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5005';
+const API_URL = import.meta.env.VITE_API_URL || 'https://local-handicraft-backend.adaptable.app';
 export { API_URL };
